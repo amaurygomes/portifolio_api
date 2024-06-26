bd291e556253bdd4de26870a3b95d8f84c3924d4
amaurygomes <amaury_mju@hotmail.com>
2024-06-26 19:33:15 -0400

    update

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4r7lr+_@g0q-@u58-0&bf4i6foga@8jx4!h4kdzyhkvk7n8qd1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
